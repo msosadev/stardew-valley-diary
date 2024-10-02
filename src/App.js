2bd009852363bdddc4a5e3e4dae9e28f2fb509ed
Miguel Sosa <msosadesign@gmail.com>
2024-10-02 23:14:15 +0200

    Move filterValues to their own file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 import bundles from './bundles.json'
+import filterValues from './filterValues.json'
 import FilterRadio from './components/FilterRadio';
 import Bundle from './components/Bundle';
 
 function App() {
   const [selectedFilter, setSelectedFilter] = useState("");
-  const filterValues = [
-    {"label": "Spring", "img": "https://stardewvalleywiki.com/mediawiki/images/thumb/9/9c/Spring.png/24px-Spring.png", "id": "spring"},
-    {"label": "Summer", "img": "https://stardewvalleywiki.com/mediawiki/images/thumb/8/85/Summer.png/24px-Summer.png", "id": "summer"},
-    {"label": "Fall", "img": "https://stardewvalleywiki.com/mediawiki/images/thumb/5/5d/Fall.png/24px-Fall.png", "id": "fall"},
-    {"label": "Winter", "img": "https://stardewvalleywiki.com/mediawiki/images/thumb/a/a7/Winter.png/24px-Winter.png", "id": "winter"}
-  ];
 
   return (
     <div className="App">
